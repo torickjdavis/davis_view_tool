c9f8cfe5355e49220cf93dad702a0c5c4598c225
Jordan Hudgens <jordan@devcamp.com>
2017-07-24 22:46:34 +0000

    DavisViewTool initial commit

---
+++ lib/davis_view_tool.rb
@@ -0,0 +1,6 @@
+require "davis_view_tool/version"
+require "davis_view_tool/renderer"
+
+module DavisViewTool
+  # Your code goes here...
+end
---
+++ lib/davis_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DavisViewTool
+  VERSION = "0.1.0"
+end
